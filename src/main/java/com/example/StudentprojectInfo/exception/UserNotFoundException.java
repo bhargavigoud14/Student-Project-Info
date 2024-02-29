a8fe225d0921c908a4c20647d30eceb6c912a9a2
Bhargavi <bhargavigoud2003@gmail.com>
2024-02-29 10:41:05 +0530

    first commit

---
+++ src/main/java/com/example/StudentprojectInfo/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.StudentprojectInfo.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
+
